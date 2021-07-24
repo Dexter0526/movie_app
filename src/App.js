1389620a429146469c403d2f40b03949b6177532
dexter <dex9205@gmail.com>
2021-07-24 14:01:22 +0900

    add props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import React from "react";
+
+function Movie({fav}) {
+  return <h2>I like {fav}</h2>;
+}
+
+// function Movie_Name({ fav }) {
+//   return <h2>I like {fav}</h2>;
+// }
+
+const movieILike = [
+  {
+    fav: "액션"
+  },
+  {
+    fav: "드라마"
+  },
+  {
+    fav: "호러"
+  }
+];
+
 function App() {
   return (
     <div>
-      Dom
+      {movieILike.map(item => (
+        <Movie fav={item.fav} />
+      ))}
     </div>
   );
 }
