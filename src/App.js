c10afc600b14f3829b5e3ceaf1a0d40324a2eecb
dexter <dex9205@gmail.com>
2021-07-24 14:28:15 +0900

    add propTypes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Movie({fav}) {
-  return <h2>I like {fav}</h2>;
+const movieILike = [
+  {
+    id: 1,
+    fav: "액션",
+    rating: 4.5,
+  },
+  {
+    id: 2,
+    fav: "드라마",
+    rating: 4.2,
+  },
+  {
+    id: 3,
+    fav: "호러",
+    rating: 4.3,
+  },
+];
+
+function Movie({ fav, rating }) {
+  return (
+    <div>
+      <h2>I like {fav}</h2>
+      <h4>{rating} / 5.0</h4>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  fav: PropTypes.string.isRequired,
+  rating: PropTypes.number
 }
 
 // function Movie_Name({ fav }) {
 //   return <h2>I like {fav}</h2>;
 // }
 
-const movieILike = [
-  {
-    fav: "액션"
-  },
-  {
-    fav: "드라마"
-  },
-  {
-    fav: "호러"
-  }
-];
-
 function App() {
   return (
     <div>
-      {movieILike.map(item => (
-        <Movie fav={item.fav} />
+      {movieILike.map((item) => (
+        <Movie key={item.id} fav={item.fav} rating={item.rating} />
       ))}
     </div>
   );
